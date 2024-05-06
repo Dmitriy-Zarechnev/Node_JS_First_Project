bcd12eec8f8169aa06aa27d733971ec04b1a5738
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-05-06 13:52:30 +0300

    06.05 - get request - 1

--- dist/index.js
+++ dist/index.js
@@ -4,11 +4,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+// create express app
 const app = (0, express_1.default)();
-const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello May!');
+const port = 5000;
+const products = [{ title: 'tomato' }, { title: 'orange' }];
+const addresses = [{ id: 1, value: 'Street 15' }, { id: 2, value: 'Street 20' }];
+app.get('/products', (req, res) => {
+    res.send(products);
 });
+app.get('/products/:productTitle', (req, res) => {
+    let product = products.find((el) => el.title === req.params.productTitle);
+    product ? res.send(product) : res.send(404);
+});
+app.get('/addresses', (req, res) => {
+    res.send(addresses);
+});
+app.get('/addresses/:id', (req, res) => {
+    let address = addresses.find((el) => el.id === +req.params.id);
+    address ? res.send(address) : res.send(404);
+});
+// start app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,45 @@
 import express, {Request, Response} from 'express'
 
+// create express app
 const app = express()
-const port = 3000
+const port = 5000
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello May!')
+
+const products: Products[] = [{title: 'tomato'}, {title: 'orange'}]
+const addresses: Addresses[] = [{id: 1, value: 'Street 15'}, {id: 2, value: 'Street 20'}]
+
+app.get('/products', (req: Request, res: Response) => {
+    res.send(products)
 })
 
+app.get('/products/:productTitle', (req: Request, res: Response) => {
+    let product = products.find((el) => el.title === req.params.productTitle)
+
+    product ? res.send(product) : res.send(404)
+})
+
+app.get('/addresses', (req: Request, res: Response) => {
+    res.send(addresses)
+})
+
+app.get('/addresses/:id', (req: Request, res: Response) => {
+    let address = addresses.find((el) => el.id === +req.params.id)
+
+    address ? res.send(address) : res.send(404)
+})
+
+// start app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+
+// types for APP
+type Products = {
+    title: string
+}
+
+type Addresses = {
+    id: number,
+    value: string
+}
