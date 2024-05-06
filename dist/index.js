e736f491b951ebfe02fac8a42caaf293f9706359
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-05-06 15:11:12 +0300

    06.05 - delete request - 1

--- dist/index.js
+++ dist/index.js
@@ -7,10 +7,16 @@ const express_1 = __importDefault(require("express"));
 // create express app
 const app = (0, express_1.default)();
 const port = 5000;
-const products = [{ title: 'tomato' }, { title: 'orange' }];
+const products = [{ id: 1, title: 'tomato' }, { id: 2, title: 'orange' }];
 const addresses = [{ id: 1, value: 'Street 15' }, { id: 2, value: 'Street 20' }];
 app.get('/products', (req, res) => {
-    res.send(products);
+    if (req.query.title) {
+        let searchString = req.query.title.toString();
+        res.send(products.filter((el) => el.title.includes(searchString)));
+    }
+    else {
+        res.send(products);
+    }
 });
 app.get('/products/:productTitle', (req, res) => {
     let product = products.find((el) => el.title === req.params.productTitle);
@@ -22,6 +28,16 @@ app.get('/addresses', (req, res) => {
 app.get('/addresses/:id', (req, res) => {
     let address = addresses.find((el) => el.id === +req.params.id);
     address ? res.send(address) : res.send(404);
+});
+app.delete('/products/:id', (req, res) => {
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id === +req.params.id) {
+            products.splice(i, 1);
+            res.send(204);
+            return;
+        }
+    }
+    res.send(404);
 });
 // start app
 app.listen(port, () => {
--- src/index.ts
+++ src/index.ts
@@ -5,27 +5,41 @@ const app = express()
 const port = 5000
 
 
-const products: Products[] = [{title: 'tomato'}, {title: 'orange'}]
+const products: Products[] = [{id: 1, title: 'tomato'}, {id: 2, title: 'orange'}]
 const addresses: Addresses[] = [{id: 1, value: 'Street 15'}, {id: 2, value: 'Street 20'}]
 
 app.get('/products', (req: Request, res: Response) => {
-    res.send(products)
+    if (req.query.title) {
+        let searchString = req.query.title.toString()
+        res.send(products.filter((el) => el.title.includes(searchString)))
+    } else {
+        res.send(products)
+    }
 })
-
 app.get('/products/:productTitle', (req: Request, res: Response) => {
     let product = products.find((el) => el.title === req.params.productTitle)
 
     product ? res.send(product) : res.send(404)
 })
-
 app.get('/addresses', (req: Request, res: Response) => {
     res.send(addresses)
 })
-
 app.get('/addresses/:id', (req: Request, res: Response) => {
     let address = addresses.find((el) => el.id === +req.params.id)
 
     address ? res.send(address) : res.send(404)
+})
+
+app.delete('/products/:id', (req: Request, res: Response) => {
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id === +req.params.id) {
+            products.splice(i, 1)
+            res.send(204)
+            return
+        }
+    }
+
+    res.send(404)
 })
 
 // start app
@@ -36,6 +50,7 @@ app.listen(port, () => {
 
 // types for APP
 type Products = {
+    id: number,
     title: string
 }
 
