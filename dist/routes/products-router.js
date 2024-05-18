44807da1c089baee82262acd1eaa07f0f18565f6
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-05-18 13:20:00 +0300

    18.05 - validation - 1

--- dist/routes/products-router.js
+++ dist/routes/products-router.js
@@ -3,7 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.productRouter = void 0;
 const express_1 = require("express");
 const products_repository_1 = require("../repositories/products-repository");
+const express_validator_1 = require("express-validator");
+const input_validation_middleware_1 = require("../middlewares/input-validation-middleware");
 exports.productRouter = (0, express_1.Router)({});
+const titleValidation = (0, express_validator_1.body)('title')
+    .trim().isLength({ min: 3, max: 10 })
+    .withMessage('Title length should be from 3 to 10 symbols');
 exports.productRouter.get('/', (req, res) => {
     var _a;
     const foundProducts = products_repository_1.productsRepository.findProducts((_a = req.query.title) === null || _a === void 0 ? void 0 : _a.toString());
@@ -19,18 +24,13 @@ exports.productRouter.get('/:id', (req, res) => {
 });
 exports.productRouter.delete('/:id', (req, res) => {
     const isDeleted = products_repository_1.productsRepository.deleteProduct(+req.params.id);
-    if (isDeleted) {
-        res.send(204);
-    }
-    else {
-        res.send(404);
-    }
+    isDeleted ? res.send(204) : res.send(404);
 });
-exports.productRouter.post('/', (req, res) => {
+exports.productRouter.post('/', titleValidation, input_validation_middleware_1.inputValidationMiddleware, (req, res) => {
     const newProduct = products_repository_1.productsRepository.createProduct(req.body.title);
     res.status(201).send(newProduct);
 });
-exports.productRouter.put('/:id', (req, res) => {
+exports.productRouter.put('/:id', titleValidation, input_validation_middleware_1.inputValidationMiddleware, (req, res) => {
     const isUpdated = products_repository_1.productsRepository.updateProduct(+req.params.id, req.body.title);
     if (isUpdated) {
         const product = products_repository_1.productsRepository.getProductByTitle(req.body.title);
--- src/routes/products-router.ts
+++ src/routes/products-router.ts
@@ -1,8 +1,14 @@
 import {Router, Request, Response} from 'express'
 import {productsRepository} from '../repositories/products-repository'
+import {body} from 'express-validator'
+import {inputValidationMiddleware} from '../middlewares/input-validation-middleware'
 
 
 export const productRouter = Router({})
+
+const titleValidation = body('title')
+    .trim().isLength({min: 3, max: 10})
+    .withMessage('Title length should be from 3 to 10 symbols')
 
 
 productRouter.get('/', (req: Request, res: Response) => {
@@ -27,30 +33,34 @@ productRouter.get('/:id', (req: Request, res: Response) => {
 productRouter.delete('/:id', (req: Request, res: Response) => {
     const isDeleted = productsRepository.deleteProduct(+req.params.id)
 
-    if (isDeleted) {
-        res.send(204)
-    } else {
-        res.send(404)
-    }
+    isDeleted ? res.send(204) : res.send(404)
 })
 
 
-productRouter.post('/', (req: Request, res: Response) => {
-    const newProduct = productsRepository.createProduct(req.body.title)
+productRouter.post('/',
+    titleValidation,
+    inputValidationMiddleware,
+    (req: Request, res: Response) => {
 
-    res.status(201).send(newProduct)
-})
+        const newProduct = productsRepository.createProduct(req.body.title)
+
+        res.status(201).send(newProduct)
+    })
 
 
-productRouter.put('/:id', (req: Request, res: Response) => {
-    const isUpdated = productsRepository.updateProduct(+req.params.id, req.body.title)
+productRouter.put('/:id',
+    titleValidation,
+    inputValidationMiddleware,
+    (req: Request, res: Response) => {
 
-    if (isUpdated) {
-        const product = productsRepository.getProductByTitle(req.body.title)
-        res.send(product)
-    } else {
-        res.send(404)
-    }
-})
+        const isUpdated = productsRepository.updateProduct(+req.params.id, req.body.title)
+
+        if (isUpdated) {
+            const product = productsRepository.getProductByTitle(req.body.title)
+            res.send(product)
+        } else {
+            res.send(404)
+        }
+    })
 
 
